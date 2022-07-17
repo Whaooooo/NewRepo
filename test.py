79d6663282da32f764a87684891454b22b2d0771
AAA <3225329724@qq.com>
2022-07-17 19:57:28 +0800

    test

--- test.py
+++ test.py
@@ -4,21 +4,21 @@ import math
 import numpy as np
 import itertools
 from matplotlib import pyplot as plt
-K = tc.set_backend("tensorflow")
-n, nlayers = 4, 2
-# number of qubits, number of layers
-
-
-def ladder_layout_circuit(params, pbc=False):
+zz = tc.gates._zz_matrix
+print("zz matrix:\n", zz)
+def brickwall_vtwo_layout_circuit(params, pbc=False):
     """
     `params` is for circuit trainable parameters
     """
     c = tc.Circuit(n)
     offset = 0 if pbc else 1
     for j in range(nlayers):
-        for i in range(n - offset):
-            c.cnot(i, (i + 1) % n)
+        for i in range(0, n - offset, 2):
+            c.exp1(i, (i + 1) % n, theta=params[j, i, 2], unitary=zz)
         for i in range(n):
-            c.rx(i, theta=params[j, i])
+            c.rx(i, theta=params[j, i, 0])
+        for i in range(1, n - offset, 2):
+            c.exp1(i, (i + 1) % n, theta=params[j, i, 2], unitary=zz)
+        for i in range(n):
+            c.rx(i, theta=params[j, i, 1])
     return c
-ladder_layout_circuit(K.ones(shape=[nlayers, n])).draw(output="mpl")
