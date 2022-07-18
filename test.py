b074bd3dd793d5d0c15b0e70fea0bcdea28ac4ea
AAA <3225329724@qq.com>
2022-07-18 19:51:48 +0800

    test

--- test.py
+++ test.py
@@ -4,21 +4,11 @@ import math
 import numpy as np
 import itertools
 from matplotlib import pyplot as plt
-zz = tc.gates._zz_matrix
-print("zz matrix:\n", zz)
-def brickwall_vtwo_layout_circuit(params, pbc=False):
-    """
-    `params` is for circuit trainable parameters
-    """
-    c = tc.Circuit(n)
-    offset = 0 if pbc else 1
-    for j in range(nlayers):
-        for i in range(0, n - offset, 2):
-            c.exp1(i, (i + 1) % n, theta=params[j, i, 2], unitary=zz)
-        for i in range(n):
-            c.rx(i, theta=params[j, i, 0])
-        for i in range(1, n - offset, 2):
-            c.exp1(i, (i + 1) % n, theta=params[j, i, 2], unitary=zz)
-        for i in range(n):
-            c.rx(i, theta=params[j, i, 1])
-    return c
+import tensorcircuit as tc
+c = tc.Circuit(2)
+c.H(0)
+c.CNOT(0,1)
+c.rx(1, theta=0.2)
+print(c.wavefunction())
+print(c.expectation_ps(z=[0, 1]))
+print(c.sample())
