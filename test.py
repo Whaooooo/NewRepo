ad5667d6cbd32c61f4c36405a61d5bb7c4b89305
AAA <3225329724@qq.com>
2022-07-26 10:16:32 +0800

    test

--- test.py
+++ test.py
@@ -5,7 +5,15 @@ import numpy as np
 import itertools
 from matplotlib import pyplot as plt
 import tensorcircuit as tc
-c = tc.Circuit(2)
+c = tc.Circuit(4)
 c.x(0)
-print(c.expectation([tc.gates.z(), [1]]))
-print(c.expectation([tc.gates.z(), [0]]))
+c.h(1)
+c.x(2)
+c.z(2)
+c.x(3)
+c.z(3)
+c.x(3)
+print(c.expectation([tc.gates.x(), [0]]))
+print(c.expectation([tc.gates.x(), [1]]))
+print(c.expectation([tc.gates.x(), [2]]))
+print(c.expectation([tc.gates.x(), [3]]))
